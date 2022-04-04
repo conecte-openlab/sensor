44ef875cb1315830f5ca773b9b4f07455e09635d
vdgaete <vdgaete@hotmail.com>
2022-04-04 14:34:03 -0300

    Atualização - Exemplo funcionando

--- ProjectIO/ESP32-SPARKFUN-ARDUINO/src/main.cpp
+++ ProjectIO/ESP32-SPARKFUN-ARDUINO/src/main.cpp
@@ -5,11 +5,60 @@
 int resPin = 4;
 int mfioPin = 5;
 
+SparkFun_Bio_Sensor_Hub bioHub(resPin, mfioPin); 
 
-void setup() {
+bioData body;  
 
+void setup(){
+
+  Serial.begin(115200);
+
+  Wire.begin();
+  int result = bioHub.begin();
+  if (result == 0) //Zero errors!
+    Serial.println("Sensor started!");
+  else
+    Serial.println("Could not communicate with the sensor!!!");
+ 
+  Serial.println("Configuring Sensor...."); 
+  int error = bioHub.configBpm(MODE_TWO); // Configuring just the BPM settings. 
+  if(error == 0){ // Zero errors
+    Serial.println("Sensor configured.");
+  }
+  else {
+    Serial.println("Error configuring sensor.");
+    Serial.print("Error: "); 
+    Serial.println(error); 
+  }
+
+  // Data lags a bit behind the sensor, if you're finger is on the sensor when
+  // it's being configured this delay will give some time for the data to catch
+  // up. 
+  Serial.println("Loading up the buffer with data....");
+  delay(4000); 
+  
 }
 
-void loop() {
 
+//Loop 
+void loop(){
+
+    // Information from the readBpm function will be saved to our "body"
+    // variable.  
+    body = bioHub.readBpm();
+    Serial.print("Heartrate: ");
+    Serial.println(body.heartRate); 
+    Serial.print("Confidence: ");
+    Serial.println(body.confidence); 
+    Serial.print("Oxygen: ");
+    Serial.println(body.oxygen); 
+    Serial.print("Status: ");
+    Serial.println(body.status); 
+    Serial.print("Extended Status: ");
+    Serial.println(body.extStatus); 
+    Serial.print("Blood Oxygen R value: ");
+    Serial.println(body.rValue); 
+    // Slow it down or your heart rate will go up trying to keep up
+    // with the flow of numbers
+    delay(2500); 
 }
